bf48f37bbb7646e0785f187402d0eeab7978b6fa
Vindicatree <119718565+Vindicatree@users.noreply.github.com>
2023-09-02 20:19:54 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
-const header = document.querySelector("header");
-const sectionOne = document.querySelector(".hero");
 
-const sectionOneOptions = {
-    rootMargin: "-500px 0px 0px 0px"
-};
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('nav');
 
-const sectionOneObserver = new IntersectionObserver(function(entries, sectionOneObserver) {
-    entries.forEach(entry=> {
-        if (!entry.isIntersecting) {
-            header.classList.add('nav_scrolled');
-        } else {
-            header.classList.remove("nav_scrolled");
-        }
-    });
-}, 
-sectionOneOptions);
-
-sectionOneObserver.observe(sectionOne);
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+}
